800a38f2eb23211600e5f10b169cda78e5f36a8e
William Amos Confer <conferw@sunypoly.edu>
2022-02-21 01:14:34 -0500

    Implement most ALU/CMP instructions

--- teenyat.c
+++ teenyat.c
@@ -5,6 +5,14 @@
 
 #include "teenyat.h"
 
+static void set_elg_flags(teenyat *t, tny_sword alu_result) {
+	t->flags.equals = (alu_result == 0);
+	t->flags.less =  alu_result & (1 << 15);
+	t->flags.greater = (alu_result > 0);
+
+	return;
+}
+
 bool tny_init_from_file(teenyat *t, FILE *bin_file) {
 	if(!t) return false;
 	t->initialized = false;
@@ -78,7 +86,7 @@ bool tny_clock(teenyat *t) {
 
  	tny_word IR = t->ram[t->reg[TNY_REG_PC].u++];
 	t->reg[TNY_REG_PC].u &= TNY_MAX_RAM_ADDRESS;
-	tny_uword immed = t->ram[t->reg[TNY_REG_PC].u++].u;
+	tny_sword immed = t->ram[t->reg[TNY_REG_PC].u++].s;
 
 	/*
 	 * We're not masking the PC immediately here, because the instruction may
@@ -102,6 +110,10 @@ bool tny_clock(teenyat *t) {
 		t->reg[TNY_REG_PC].u--;
 		immed = IR.instruction.immed4;
 	}
+	else {
+		/* double word instructions cost one extra cycle */
+		t->delay_cycles++;
+	}
 	/* mask the PC into range, whether teeny or not */
 	t->reg[TNY_REG_PC].u &= TNY_MAX_RAM_ADDRESS;
 
@@ -109,14 +121,179 @@ bool tny_clock(teenyat *t) {
 	 * EXECUTE
 	 */
 
+	uint32_t tmp;  /* for quick use to determine carry */
+	bool bus_access = false;
+
 	switch(opcode) {
+	case TNY_OPCODE_SET:
+		t->reg[reg1].s = t->reg[reg2].s + immed;
+		break;
+	case TNY_OPCODE_LOD:
+		/***************************************/
+		t->reg[reg1].s = t->ram[t->reg[reg2].s + immed].s;
+		break;
+	case TNY_OPCODE_STR:
+		/***************************************/
+		{
+			tny_uword addr = t->reg[reg1].s + immed;
+			if(addr > TNY_MAX_RAM_ADDRESS) {
+				//t->state
+			}
+		}
+		break;
+	case TNY_OPCODE_PSH:
+		/***************************************/
+		break;
+	case TNY_OPCODE_POP:
+		/***************************************/
+		break;
+	case TNY_OPCODE_BTS:
+		{
+			tny_sword bit = t->reg[reg2].s + immed;
+			if(bit >= 0 && bit <= 15) {
+				t->reg[reg2].s |= (1 << bit);
+			}
+		}
+		break;
+	case TNY_OPCODE_BTC:
+		{
+			tny_sword bit = t->reg[reg2].s + immed;
+			if(bit >= 0 && bit <= 15) {
+				t->reg[reg2].s &= ~(1 << bit);
+			}
+		}
+		break;
+	case TNY_OPCODE_BTF:
+		{
+			tny_sword bit = t->reg[reg2].s + immed;
+			if(bit >= 0 && bit <= 15) {
+				t->reg[reg2].s ^= (1 << bit);
+			}
+		}
+		break;
+	case TNY_OPCODE_CAL:
+		t->reg[TNY_REG_SP].u &= TNY_MAX_RAM_ADDRESS;
+		t->ram[t->reg[TNY_REG_SP].u] = t->reg[TNY_REG_PC];
+		t->reg[TNY_REG_SP].u--;
+		t->reg[TNY_REG_SP].u &= TNY_MAX_RAM_ADDRESS;
+		t->reg[TNY_REG_PC].u = (t->reg[reg2].s + immed) & TNY_MAX_RAM_ADDRESS;
+		break;
+	case TNY_OPCODE_ADD:
+		tmp = (uint32_t)(t->reg[reg1].s) + (uint32_t)((uint32_t)(t->reg[reg2].s) + (uint32_t)immed);
+		t->flags.carry = tmp & (1 << 16);
+		t->reg[reg1].s = tmp;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_SUB:
+		tmp = (uint32_t)(t->reg[reg1].s) - (uint32_t)((uint32_t)(t->reg[reg2].s) + (uint32_t)immed);
+		t->flags.carry = tmp & (1 << 16);
+		t->reg[reg1].s = tmp;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_MPY:
+		tmp = (uint32_t)(t->reg[reg1].s) * (uint32_t)((uint32_t)(t->reg[reg2].s) + (uint32_t)immed);
+		t->flags.carry = tmp & (1 << 16);
+		t->reg[reg1].s = tmp;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_DIV:
+		t->reg[reg1].s /= t->reg[reg2].s + immed;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_MOD:
+		t->reg[reg1].s %= t->reg[reg2].s + immed;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_AND:
+		t->reg[reg1].s &= t->reg[reg2].s + immed;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_OR:
+		t->reg[reg1].s |= t->reg[reg2].s + immed;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_XOR:
+		t->reg[reg1].s ^= t->reg[reg2].s + immed;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_SHF:
+		{
+			tny_sword bits_to_shift = t->reg[reg2].s + immed;
+			if(bits_to_shift > 0) {
+				if(bits_to_shift <= 15) {
+					t->reg[reg1].u <<= bits_to_shift - 1;
+					t->flags.carry = t->reg[reg1].u & (1 << 15);
+					t->reg[reg1].u <<= 1;
+				}
+				else {
+					if(bits_to_shift == 16) {
+						t->flags.carry = t->reg[reg1].u & (1 << 0);
+					}
+					else {
+						t->flags.carry = 0;
+					}
+					t->reg[reg1].u = 0;
+				}
+			}
+			else if(bits_to_shift < 0) {
+				bits_to_shift *= -1;
+				if(bits_to_shift <= 15) {
+					t->reg[reg1].u >>= bits_to_shift - 1;
+					t->flags.carry = t->reg[reg1].u & (1 << 0);
+					t->reg[reg1].u >>= 1;
+				}
+				else {
+					if(bits_to_shift == 16) {
+						t->flags.carry = t->reg[reg1].u & (1 << 15);
+					}
+					else {
+						t->flags.carry = 0;
+					}
+					t->reg[reg1].u = 0;
+				}
+			}
+			set_elg_flags(t, t->reg[reg1].s);
+		}
+		break;
+	case TNY_OPCODE_ROT:
+
+
+
+
+
+		/* TODO */
+
+
+
+
+
+
+		break;
+	case TNY_OPCODE_NEG:
+		tmp = (uint32_t)0 - (uint32_t)(t->reg[reg1].s);
+		t->flags.carry = tmp & (1 << 16);
+		t->reg[reg1].s = tmp;
+		set_elg_flags(t, t->reg[reg1].s);
+		break;
+	case TNY_OPCODE_CMP:
+		tmp = (uint32_t)(t->reg[reg1].s) - (uint32_t)((uint32_t)(t->reg[reg2].s) + (uint32_t)immed);
+		t->flags.carry = tmp & (1 << 16);
+		set_elg_flags(t, (tny_sword)tmp);
+		break;
+	case TNY_OPCODE_JMP:
+
+		break;
+	case TNY_OPCODE_DJZ:
+
+		break;
+
 	default:
 		fprintf(stderr, "Uknown opcode (%d) encountered at 0x%04X on cycle %llu\n",
 		        opcode, orig_PC, t->cycle_cnt);
 		break;
 	}
 
-	return false;
+	return bus_access;
 }
 
 void tny_lod_result(teenyat *t, tny_word data, uint16_t delay) {
