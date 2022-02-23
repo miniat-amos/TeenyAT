ccc4f4a80c542b331614df5ac7abbc02d03c972b
William "Amos" Confer <conferw@sunypoly.edu>
2022-02-23 14:12:04 -0500

    Make some instruction examples more unique to show variation

--- syntax.asm
+++ syntax.asm
@@ -6,14 +6,29 @@
         ; follow.  Labels start with a "!" and are then followed by non-
         ; whitespace and non-"!" characters.
 
-;;;;;;;;; ALU Instructions ;;;;;;;;;
+;--- Registers ---
+; the eight registers supported by tnasm are:
+; rA, rB, rC, rD, rE: five general purpose registers
+; PC: the Program Counter register
+; SP: the Stack Pointer register
+; rZ: the Zero Register (always contains the value zero)
+
+;--- Common Instruction formats
+; The great majority of instructions have three main forms supported by tnasm.
+; These forms are differentiated by whether a particular operand is a register,
+; an immediate, or a register added to an immediate.  Not all instructions can
+; be written in each of the three forms, but for the sake of clarity, the
+; examples in the remainder of this file show ALL acceptable forms for every
+; instruction.
+
+;--- ALU Instructions ---
 add rA, rB        ; same as... rA += rB
 add rA, 72        ; same as... rA += 72
 add rA, rC + 13   ; same as... rA += rC + 13
 
-sub rA, rB        ; same as... rA -= rB
-sub rA, 72        ; same as... rA -= 72
-sub rA, rC + 13   ; same as... rA -= rC + 13
+sub rD, rE        ; same as... rD -= rE
+sub SP, 4        ; same as... SP -= 4
+sub PC, rB - 5   ; same as... PC -= rB - 5
 
 mpy rA, rB        ; same as... rA *= rB
 mpy rA, 72        ; same as... rA *= 72
@@ -51,6 +66,6 @@ rol rA, rB        ; same as... rA = (rA << rB) | (rA >> (16 - rB)
 rol rA, 4        ; same as... rA = (rA << 4) | (rA >> (16 - 4)
 rol rA, rC + 13   ; same as... rA = (rA << (rC + 13)) | (rA >> (16 - (rA + 13)))
 
-rol rA, rB        ; same as... rA += rB
-rol rA, 72        ; same as... rA += 72
-rol rA, rC + 13   ; same as... rA += rC + 13
+ror rA, rB        ; same as... rA = (rA >> rB) | (rA << (16 - rB)
+ror rA, 4        ; same as... rA = (rA >> 4) | (rA << (16 - 4)
+ror rA, rC + 13   ; same as... rA = (rA >> (rC + 13)) | (rA << (16 - (rA + 13)))
