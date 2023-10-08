694d738833aee06e09970f1de84f470e59858816
Aero Griffin <adevgriff@gmail.com>
2023-10-08 11:07:46 -0400

    Finished barebones level of trash untested on VM

---
+++ testSystem/asmblr/headers/grammarFunctions.hpp
@@ -0,0 +1,39 @@
+#ifndef GRAMMARFUNCTIONS__H
+#define GRAMMARFUNCTIONS__H
+
+#include <vector>
+#include <sstream>
+#include <map>
+#include <cstdint>
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <algorithm>
+
+#include "teenyat.h"
+
+void append32ToFile(const std::string& filename, uint32_t value);
+
+void append16ToFile(const std::string& filename, uint16_t value);
+
+extern void grammar0(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar1(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar2(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar3(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar4(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar5(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar6(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar7(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar8(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar9(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar10(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar11(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar12(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar13(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar14(const std::vector<std::string>& line, const std::string& filename);
+
+extern void grammar32(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar33(const std::vector<std::string>& line, const std::string& filename);
+extern void grammar34(const std::vector<std::string>& line, const std::string& filename);
+
+#endif//GRAMMARFUNCTIONS__H
