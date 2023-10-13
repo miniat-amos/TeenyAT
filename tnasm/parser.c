c0f3ec8215a5e83c2a28a22a16a320f4dd5a952c
William "Amos" Confer <conferw@sunypoly.edu>
2023-10-12 23:39:58 -0400

    Establish base pattern for recursive descent parser

---
+++ tnasm/parser.c
@@ -0,0 +1,34 @@
+#include "../token.h"
+#include "parser.h"
+
+using namespace std;
+
+Token_Line parse_line;  // The current line of tokens being parsed
+int i next;  // index of the next token in the line to consider
+
+bool p_variable_line();
+bool p_constant_line();
+bool p_raw_line();
+bool p_label_line();
+
+bool p_immed();
+
+bool term(Token_Type id) {
+    return next < parse_line.size() && parse_line[next++].id == id;
+}
+
+bool p_loc() {
+    int save = next;
+    return (next = save, p_variable_line()) ||
+           (next = save, p_constant_line()) ||
+           (next = save, p_raw_line()) ||
+           (next = save, p_label_line());
+}
+
+bool p_variable_line() {
+    return term(T_VARIABLE) && term(T_IDENTIFIER) && p_immed() && term(T_EOL);
+}
+
+bool p_constant_line() {
+    return term(T_CONSTANT) && term(T_IDENTIFIER) && p_immed() && term(T_EOL);
+}
---
+++ tnasm/parser.h
@@ -0,0 +1,8 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include "../token.h"
+
+
+
+#endif /* __PARSER_H__ */
