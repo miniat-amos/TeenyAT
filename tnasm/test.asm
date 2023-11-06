dbd55439e01ce22edd47c94929bc2762702994d1
William Amos Confer <conferw@sunypoly.edu>
2023-11-06 01:38:47 -0500

    Add a test assembly file to see how tnasm is coming

---
+++ tnasm/test.asm
@@ -0,0 +1,15 @@
+; just a test assembly file to see how things are going
+
+.const thirty_one 31
+
+!main
+    add r3, r4 + 7
+    OR  SP, rA - 5
+    div rE, 15
+    MpY rC, pc
+
+!more
+    BTF r1, r3 + thirty_one
+
+
+; and that's all
