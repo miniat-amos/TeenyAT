40f4e09b40bb89b960623e6da2f46c4fa2434b9d
Noah Breedy <breedyn@sunypoly.edu>
2023-11-30 10:48:14 -0500

    Added Mouse constanst to ex11

--- lcd/asm/ex11_mouse.asm
+++ lcd/asm/ex11_mouse.asm
@@ -19,6 +19,14 @@
 .const TERM 0xFFFF
 .const KEY 0xFFFE
 
+.const MOUSE_LEFT 1
+.const MOUSE_CENTER 2
+.const MOUSE_RIGHT 4
+.const MOUSE_LR 5
+.const MOUSE_LC 3
+.const MOUSE_RC 6
+.const MOUSE_LRC 7 
+
 .const MUTATEAMT 50
 .const CHANGEAMT 50
 
@@ -28,16 +36,19 @@ SET rD, 0
     LOD rB, [MOUSEY]
     LOD rC, [MOUSEB]
 
-    CMP rC, 1 ; left down
+    CMP rC, MOUSE_LEFT ; left down
     JE !paint
 
-    CMP rC, 2 ; middle down
+    CMP rC, MOUSE_CENTER ; middle down
     JE !mutate
 
-    CMP rC, 4 ; right  down
+    CMP rC, MOUSE_RC ; right and middle down
+    JE !resetScreen
+
+    CMP rC, MOUSE_RIGHT ; right  down
     JE !changecolor
 
-    CMP rC, 5 ; left and right down
+    CMP rC, MOUSE_LR ; left and right down
     JE !changecolor_and_paint
 
     JMP !main
@@ -68,7 +79,6 @@ SET rD, 0
 !mutate
     SET rA, 0
     !loop
-
         LOD rC, [MOUSEB]
         CMP rC, 2 ; middle down
         JNE !main
@@ -81,5 +91,21 @@ SET rD, 0
         JG !end
         JMP !loop
     !end
+        STR [UPDATE],rZ
+        JMP !main
+
+!resetScreen
+    SET rA, 0
+    LOD rC, [MOUSEB]
+    CMP rC, MOUSE_RC ; so they cant just keep resetting it over and over
+    JNE !main
+    !reset_screen_loop    
+        SET rE, 2903 ; our original lavender color
+        STR [rA + UPDATESCREEN], rE
+        INC rA
+        CMP rA, 0x1000
+        JG !reset_end
+        JMP !reset_screen_loop
+    !reset_end
         STR [UPDATE],rZ
         JMP !main
