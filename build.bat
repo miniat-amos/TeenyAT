901debf94487acd600ac134f62e56a1b1428a749
William "Amos" Confer <conferw@sunypoly.edu>
2025-09-23 22:28:21 -0400

    Fixed variable usage in Windows build script

--- build.bat
+++ build.bat
@@ -3,6 +3,39 @@ setlocal
 
 pushd .
 
+
+set BUILD_DIR=build
+set BUILD_TYPE=Release
+
+if "%1"=="debug" (
+    echo Configuring for Debug build...
+    :: unsure if we want separate directories for this
+    :: set BUILD_DIR=build_debug
+    set BUILD_TYPE=Debug
+) else if "%1"=="clean" (
+    echo Cleaning build directory...
+    :: again, unsure if we want separate directories for this
+    :: rd /s /q build build_debug
+    rd /s /q build
+
+    popd
+    exit /b
+) else if "%1" neq "" (
+    echo.
+    echo Invalid parameter: %1
+    echo Usage: build.bat [debug ^| clean]
+    echo     No parameter - Default Release build
+    echo     debug - Build with debug symbols
+    echo     clean - Remove build directory
+    echo.
+
+    popd
+    exit /b
+) else (
+    echo Using default build configuration...
+)
+
+
 :: check for and prefer GCC/G++
 echo Checking for GCC/G++
 where gcc >nul 2>&1
@@ -37,37 +70,6 @@ exit /b
 :end_compiler_check
 
 
-set BUILD_DIR=build
-set BUILD_TYPE=Release
-
-if "%1"=="debug" (
-    echo Configuring for Debug build...
-    :: unsure if we want separate directories for this
-    :: set BUILD_DIR=build_debug
-    set BUILD_TYPE=Debug
-) else if "%1"=="clean" (
-    echo Cleaning build directory...
-    :: again, unsure if we want separate directories for this
-    :: rd /s /q build build_debug
-    rd /s /q build
-
-    popd
-    exit /b
-) else if "%1" neq "" (
-    echo.
-    echo Invalid parameter: %1
-    echo Usage: build.bat [debug ^| clean]
-    echo     No parameter - Default Release build
-    echo     debug - Build with debug symbols
-    echo     clean - Remove build directory
-    echo.
-
-    popd
-    exit /b
-) else (
-    echo Using default build configuration...
-)
-
 if not exist "%BUILD_DIR%" (
     echo Creating directory: "%BUILD_DIR%"
     mkdir "%BUILD_DIR%"
@@ -77,8 +79,8 @@ cd "%BUILD_DIR%"
 
 echo Running CMake configuration...
 set MAKE_TARGET=-G "MinGW Makefiles"
-set C_COMPILER=-DCMAKE_C_COMPILER="!C!"
-set CXX_COMPILER=-DCMAKE_CXX_COMPILER="!CXX!"
+set C_COMPILER=-DCMAKE_C_COMPILER="%C%"
+set CXX_COMPILER=-DCMAKE_CXX_COMPILER="%CXX%"
 set BUILD=-DCMAKE_BUILD_TYPE="%BUILD_TYPE%"
 cmake %MAKE_TARGET% %C_COMPILER% %CXX_COMPILER% %BUILD% ..
 
